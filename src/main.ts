824d8885d0b2eb015e68301b0991b1e8c1d57f45
hossamnasser938 <hossamnasser938@gmail.com>
2023-08-18 00:06:43 +0300

    include port in env variables

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,22 @@
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory, Reflector } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const reflector = app.get(Reflector);
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<number>('PORT');
+
   app.useGlobalInterceptors(new ClassSerializerInterceptor(reflector));
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
-  await app.listen(3000);
+
+  await app.listen(port);
 }
 bootstrap();
