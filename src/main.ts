e3d4ba9e47366579f1898a42ce80a6bd78cf16be
hossamnasser938 <hossamnasser938@gmail.com>
2023-08-19 23:38:29 +0300

    move from db synchronize option to migrations

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
+import { DataSource } from 'typeorm';
 
 import { AppModule } from './app.module';
 import { ConfigWrapperService } from './config-wrapper/config-wrapper.service';
@@ -8,12 +9,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const reflector = app.get(Reflector);
+  const dataSource = app.get(DataSource);
   const configWrapperService = app.get(ConfigWrapperService);
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(reflector));
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
+
+  await dataSource.runMigrations();
 
   await app.listen(configWrapperService.PORT);
 }
