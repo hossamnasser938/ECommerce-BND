38512d67bc96478f54d52d9d209e934e51f05768
hossamnasser938 <hossamnasser938@gmail.com>
2023-09-09 07:39:07 +0300

    us fs file service in development and gcloud file service in production

--- src/file-storage/file-storage-module.ts
+++ src/file-storage/file-storage-module.ts
@@ -2,13 +2,17 @@ import { Module } from '@nestjs/common';
 
 import { FileStorageController } from './file-storage.controller';
 import { FILE_STOREAGE_SERVICE_PROVIDER_TOKEN } from './file-storeage.constants';
+import { FSFileStorageService } from './fs-file-storage.service';
 import { GCloudFileStorageService } from './gcloud-file-storage.service';
 
 @Module({
   providers: [
     {
       provide: FILE_STOREAGE_SERVICE_PROVIDER_TOKEN,
-      useClass: GCloudFileStorageService,
+      useClass:
+        process.env.NODE_ENV === 'production'
+          ? GCloudFileStorageService
+          : FSFileStorageService,
     },
   ],
   exports: [FILE_STOREAGE_SERVICE_PROVIDER_TOKEN],
